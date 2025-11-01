0ed544c29849b5b072a3fd28b336024daf2da47f
Martin Castro <jmartincastroa@gmail.com>
2025-11-01 15:01:29 -0500

    fix

--- src/pages/ScheduleConfig.jsx
+++ src/pages/ScheduleConfig.jsx
@@ -27,7 +27,7 @@ const INTERVALOS = [
   { value: 30, label: '30 minutos' },
   { value: 45, label: '45 minutos' },
   { value: 60, label: '1 hora' },
-  { value: 90, label: '1 hora 30 minutos' },
+  { value: 90, label: '1 hora 1/2' },
   { value: 120, label: '2 horas' }
 ];
 
